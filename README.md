7eb63e8b4ba5d497df4741438d1ef050e2a0b959
adwinugroho <adwinnugroho16@gmail.com>
2024-10-15 09:25:17 +0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Base
+
+- This config neovim based on nvchad https://github.com/NvChad/nvchad
+
+
+# Credits
+
+- Lazyvim starter https://github.com/LazyVim/starter as nvchad's starter was inspired by Lazyvim's. It made a lot of things easier!
---
+++ lua/plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function ()
+      return require "configs.null-ls"
+    end,
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
