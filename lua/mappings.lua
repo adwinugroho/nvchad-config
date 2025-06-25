c32093e4d04218c5da7b3e2cd225ef0f2ce17b37
adwinugroho <adwinnugroho16@gmail.com>
2025-06-25 14:17:57 +0700

    fix: add simple exit terminal mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode", noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
